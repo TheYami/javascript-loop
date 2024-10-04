028eed93c11b01cc140adf29ff7b2ecc97a697f6
Sirawit-Boon <ter.sirawit1997@gmail.com>
2024-10-04 14:35:37 +0700

    assign loop ex1-6

--- ex-1.js
+++ ex-1.js
@@ -2,3 +2,10 @@
 let scores = [89, 70, 60, 50, 100, 71];
 
 // Start coding here
+let newScore = [];
+for (let i = 0; i < scores.length; i++) {
+    let score = scores[i] - 10;
+    newScore.push(score)
+}
+
+console.log(newScore);
--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,11 @@
 // Exercise #5: Concatenate the Alphabets
 let companyName = "TechUp";
-
+let numbers = 0;
 // Start coding here
+for (let letter of companyName) {
+    if (numbers < companyName.length) {
+        numbers++
+        console.log(numbers)
+    }
+    console.log(`Number ${numbers} character is ${letter}`)
+}
