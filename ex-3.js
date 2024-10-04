0292b2fce7362ec6b88ffae5b8813c1f73d270c2
TheYami <luffy90san@gmail.com>
2024-10-04 13:20:19 +0700

    Find a Minimum Score

--- ex-3.js
+++ ex-3.js
@@ -3,4 +3,13 @@ let scores = [100, 20, 3, 1000];
 let minScore;
 // Start coding here
 
+for(let i = 0; i < scores.length; i++){
+    if(!minScore){
+        minScore = scores[i]
+       
+    }else if(scores[i] < minScore){
+        minScore = scores[i]
+    }
+}
+
 console.log(minScore);
